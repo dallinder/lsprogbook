74fc698ef945fd669890822cc94e7a5889a72e65
Dan Allinder <dcallinder1s@gmail.com>
2017-06-04 20:05:31 -0600

    first 4 exercies from ls prog book

---
+++ Chapter 1/ex3.rb
@@ -0,0 +1,8 @@
+movies = {:wonderwoman => '2017',
+          :theincredibles => '2004', 
+          :meninblack => '1997',
+          :beetlejuice => '1988'}
+puts movies[:wonderwoman]
+puts movies[:theincredibles]
+puts movies[:meninblack]
+puts movies[:beetlejuice]
---
+++ Chapter 1/ex4.rb
@@ -0,0 +1,5 @@
+years = [2017, 2004, 1997, 1988]
+puts years[0]
+puts years[1]
+puts years[2]
+puts years[3]
