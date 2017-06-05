19050ff92e2db7e80223184ac45288fe7bf157d2
Dan Allinder <dcallinder1s@gmail.com>
2017-06-04 20:51:59 -0600

    finished variables section

---
+++ Variables/name.rb
@@ -0,0 +1,8 @@
+puts "Hi there! What is you're first name?"
+first_name = gets.chomp
+puts "And what is your last name?"
+last_name = gets.chomp
+
+puts "So you're name is " + first_name + " " + last_name + "."
+
+
